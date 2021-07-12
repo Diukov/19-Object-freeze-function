04351736b4da0ef2562d52f9e3a0fcb4aac93136
Dmitriy Diukov <diukovdmitriy@gmail.com>
2021-07-12 22:01:51 +0300

    object freeze function / syntax update

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 const family = {
-  description: "my family",
-  me: {
-    name: "Dmitriy",
-    age: 22,
-    nationality: "ukrainian"
-  },
-  parents: {
-    mother: {
-      name: "Tatyana",
-      age: 50,
-      nationality: "ukrainian"
+    description: 'my family',
+    me: {
+        name: 'Dmitriy',
+        age: 22,
+        nationality: 'ukrainian'
     },
-    father: {
-      name: "Valentin",
-      age: 52,
-      nationality: "russian"
-    }
-  },
-  pet: {
-    name: "Barsik",
-    age: 2,
-    nationality: "cat"
-  },
+    parents: {
+        mother: {
+            name: 'Tatyana',
+            age: 50,
+            nationality: 'ukrainian'
+        },
+        father: {
+            name: 'Valentin',
+            age: 52,
+            nationality: 'russian'
+        }
+    },
+    pet: {
+        name: 'Barsik',
+        age: 2,
+        nationality: 'cat'
+    },
 };
 
 function deepFreeze(obj) {
-  for (const key in obj) {
-    if (obj[key] !== null && typeof obj[key] === "object") {
-      deepFreeze(obj[key])
+    for (const key in obj) {
+        if (obj[key] !== null && typeof obj[key] === 'object') {
+            deepFreeze(obj[key]);
+        }
     }
-  }
-  return Object.freeze(obj)
+    return Object.freeze(obj);
 }
 
 deepFreeze(family);
